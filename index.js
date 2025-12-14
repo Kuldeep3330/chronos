5d30ea5feed75366d8f603d4ff2115563d66c8af
Kuldeep3330 <upsainian3330@gmail.com>
2025-12-14 08:30:22 +0530

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-console.log('hello world');
-console.log('hello world');console.log('hello world');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const config = require('./config');
+const jobsRouter = require('./routes/jobs');
+const systemRouter = require('./routes/system');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/jobs', jobsRouter);
+app.use('/system', systemRouter);
+
+mongoose.connect(config.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Mongo connected'))
+  .catch(err => console.error('Mongo error', err));
+
+const port = config.PORT;
+app.listen(port, () => console.log(`API listening on ${port}`));
